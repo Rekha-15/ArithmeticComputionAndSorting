fa1ae6ad33b2a1e9a481a5ef6fc177c217009dae
Rekha-15 <rekhapatil.1509@gmail.com>
2021-05-01 17:20:09 -0400

    add: AithmeticComputation.sh

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,2 +1,9 @@
 #!/bin/bash -x
 echo " Welcome to the ArithmeticComputation"
+echo "Enter the first Number: "
+read a
+echo "Enter the second number: "
+read b
+echo "Enter the third number: "
+read b
+Echo " The values are $a $b $c "
